6ee73e4549165b8dbfd230508e757810c2e39e40
heldiapriadi <54462847+heldiapriadi@users.noreply.github.com>
2020-02-20 16:10:29 +0700

    Snake Ari

--- Tester.cpp
+++ Tester.cpp
@@ -5,29 +5,16 @@
 #include <time.h>
 #include <iostream>
 
-
-#define LEFT 4
-#define RIGHT 2
-#define UP 1
-#define BOTTOM 3
-
 using namespace std;
 
 
-//Snake position
-int positionX=20,positionY=20;
-int direction;
-
-//Value of body snake
-
-
-
+int positionX=5,positionY=5,veloX=1,veloY=0;
+int xHapus=3,yHapus=3,ekorsX[200], ekorsY[200],nEkor = 2;
 COORD coord={0,0};
 void gotoxy(int x,int y)
 {
-    coord.X=x;
-    coord.Y=y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+    COORD pos = {x, y};  
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); 
 }
 
 void setcolor (unsigned short color)
@@ -36,145 +23,95 @@ void setcolor (unsigned short color)
     SetConsoleTextAttribute(hCon,color);
 }
 
-struct Snake{
-	//menampung integer umur
-	int value;
-	//menampung alamat dari data selanjutnya dan sebelumnya
-	Snake *next, *prev;
-}*head, *tail, *current;
+void tampil() 
+{  
+	gotoxy(positionX,positionY); //iX dan iY adalah posisi gambar kepala  
+	cout << char(254); //untuk menggambar gambar kepala   
+	//nilai makX dan makY adalah random  
+} 
 
-void pushTail(int value){
-	//membuat blok data baru
-	current = (struct Snake *)malloc(sizeof (Snake));
-	//mengisi data
-	current->value = value;
-	//membuat penunjuk data lain menjadi NULL terlebih dahulu
-	current->prev = current->next = NULL;
+void ganti_posisi() //mengganti posisi dari ekor ekor snake 
+{  
+	xHapus = ekorsX[nEkor - 1];  
+	yHapus = ekorsY[nEkor - 1];  
+	for(int j = nEkor - 1; j >= 1; j--)  
+	{	   
+		ekorsX[j] = ekorsX[j-1];   
+		ekorsY[j] = ekorsY[j-1];  
+	}   
+	ekorsX[0] = positionX;   
+	ekorsY[0] = positionY; 
+} 
 
-	//jika tidak ada data
-	if(head==NULL){
-		//maka akan jadi data pertama, head dan tail akan sama dengan data baru
-		head=tail=current;
-	//jika ada data
-	}else{
-		//pergantian tail menjadi data terbaru
-		current->prev = tail;
-		tail->next = current;
-		tail = current;
-	}
+void hapus() 
+{  
+	gotoxy(xHapus, yHapus); //untuk menghapus bagian yang sudah dilewati  
+	cout << " "; 
+} 
+
+void velo() //fungsi untuk melakukan update posisi snake sesuai tombol 
+//yang ditekan 
+{      
+	ganti_posisi();
+	hapus();
+	positionX += veloX;  
+	positionY += veloY;  
+	tampil(); 
+} 
+
+void atas() //untuk gerak Snake ke atas 
+{   
+	veloY = -1;
+	veloX = 0; 
+} 
+
+// untuk ular kebawah
+void bawah() 
+{   
+	veloY = 1; 
+	veloX = 0; 
+} 
+
+// untuk ular ke samping kiri
+void kiri() 
+{   
+	veloX = -1; 
+	veloY = 0; 
+} 
+
+// untuk ular ke samping kanan
+void kanan() 
+{   
+	veloX = 1; 
+	veloY = 0; 
+} 
+void controller(){
+	for(int i = 8; i <= 222; i++)  
+	{   
+		if(GetAsyncKeyState(i) == -32767)   
+		{    
+			switch(i)    
+			{		     
+				case 38 : if(veloY != 1)atas();              
+				break;
+				case 40 : if(veloY != -1)bawah();               
+				break;
+				case 37 : if(veloX != 1)kiri();               
+				break;
+				case 39 : if(veloX != -1)kanan();               
+				break;
+			}   
+		}  
+	} 
 }
 
 
-
-void move(int dx, int dy)
-{
-	int newx = positionX + dx;
-	int newy = positionY + dy;
-	
-	positionX = newx;
-	positionY = newy;
-}
-
-// Initializes game
-void InitGame()
-{
-	for(int i=0;i<3;i++)
-	{
-		cout << "O";
-	}
-}
-
-void SnakeRun()
-{
-//	xHapus = ekorsX[nEkor - 1];  
-//	yHapus = ekorsY[nEkor - 1];  
-//	for(int j = nEkor - 1; j >= 1; j--)  
-//	{	   
-//		ekorsX[j] = ekorsX[j-1];   
-//		ekorsY[j] = ekorsY[j-1];  
-//	}   
-//	ekorsX[0] = iX;   
-//	ekorsY[0] = iY;
-}
-
-void update()
-{
-	switch (direction) {
-    case UP:  move(0, -1); //up
-        break;
-    case RIGHT: move(2, 0); //right
-        break;
-    case BOTTOM: move(0, 1); //bottom
-        break;
-    case LEFT: move(-2, 0); //left
-        break;
-    }	
-}
-
-void Controller (char key)
-{
-	/*
-      W
-    A + D
-      S
-
-      1
-    4 + 2
-      3
-    */
-			switch(key)
-			{
-				case 's' :
-				{
-					if(direction!=UP) direction =  3;
-					break;
-				} //down
-				case 'w' :
-				{
-					if(direction!=BOTTOM) direction = 1;
-					break;
-				} //up
-				case 'a' :
-				{
-					if(direction!=RIGHT) direction = 4;
-					break;
-				} //left
-				case 'd' :
-				{
-					if(direction!=LEFT) direction = 2;
-					break;
-				} //right
-			}
-}
-
-void Start()
-{
-	Controller('d');
-	gotoxy(20,20);
-	InitGame();
-	while(1)
-	{
-		if (kbhit()) {
-            // Change to direction determined by key pressed
-            (Controller(getch()));
-        }
-        update();
-//        gotoxy(positionX,positionY);
-//        cout << "O";
-		
-        if(direction == UP || direction == BOTTOM)
-        {
-        	Sleep(150);
-		}
-		else
-		{
-			Sleep(100);	
-		}
-		
-	}
-}
 
 int main()
 {
-	Start();
+	while(1){
+		velo();
+		controller();
+		Sleep(90);
+	}
 }
